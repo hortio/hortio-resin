c1076bf318c901ac90fc4bae105b4087cbd1a80c
Sergei Silnov <po@kumekay.com>
2018-09-06 18:40:07 +0300

    Add dashboard to this repo

---
+++ dashboard/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hortio Web Dashboard</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
